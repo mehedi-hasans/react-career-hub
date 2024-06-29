3f69c466b19bc506f9d6a6e7e1da8fdae0d16623
Mehedi Hasan <shihab3676@gmail.com>
2024-06-29 11:39:09 +0600

    udated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ export default function Banner() {
   return (
     <div className="text-center grid gap-5 grid-cols-1 md:grid-cols-2">
         <div className="space-y-3 text-left my-20">
-          <h1 className="md:text-7xl text-5xl font-bold ">One Step <br/> Closer To Your <br/> <span className="text-sky-500">Dream Job</span></h1>
+          <h1 className="md:text-7xl text-5xl font-bold ">One Step01 <br/> Closer To Your <br/> <span className="text-sky-500">Dream Job</span></h1>
           <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
           <button className="btn main-gradient py-2 px-5 text-white">Get Started</button>
         </div>
