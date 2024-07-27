2a685d6629e3dcc92866585f2321c514ba9eee70
Mehedi Hasan <shihab3676@gmail.com>
2024-07-27 11:30:18 +0600

    customized

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -9,7 +9,7 @@ export default function ErrorPage() {
         <Header></Header>
       </div>
     <div className="w-3/12 mx-auto my-48">
-        <h1 className="text-2xl mb-5 font-bold">Oops Sorry, Page not found!!!</h1>
+        <h1 className="text-2xl mb-5 font-bold">Oops Sorry, Page not found!!!!</h1>
         <Link className="p-4 rounded-lg border block w-4/6 mx-auto text-center" to='/'>Go Back to Home</Link>
     </div>
     <Footer></Footer>
